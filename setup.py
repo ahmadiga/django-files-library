c64a4f454312a15bc4561f5a524af93718fed30c
ahmadiga <a.bazadough@sit-mena.com>
2018-02-10 12:58:14 +0200

    pip configuration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -10,8 +10,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-files-library',
     description='django files Library component',
-    version="0.01",
-    long_description=open('README.md').read(),
+    version="0.01.03",
+    long_description=open('README.rst').read(),
     author='Ahmad Bazadough',
     author_email='ahmadiga1985@hotmail.com',
     url='https://github.com/ahmadiga/django-files-library',
